5a8ff6a8548abfbe20f0334db695c2c298a6aa0f
Kamil <k.aschirov@yandex.ru>
2026-07-08 05:13:08 +0400

    Только src и pom.xml

--- src/main/java/com/example/main.java
+++ src/main/java/com/example/main.java
@@ -1,7 +1,7 @@
 import static com.example.util.StringUtil.reverseLetters;
 
 public static void main(String[] args){
-    String str = "!2Good#game$$$";
+    String str = "W@S@S@$";
     System.out.println(reverseLetters(str));
 
 }
